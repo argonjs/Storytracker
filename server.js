55ee3ac1272c004f5a6793693b0306089686a9ab
evanyui <evanyui@hotmail.com>
2017-12-04 04:38:01 -0500

    sql stores array of points, points are displayed on list, click point to draw marker on map

--- server.js
+++ server.js
@@ -25,23 +25,21 @@ app.use(express.static(__dirname + "/"));
 io.on('connection', function(socket) {
 	console.log('connected!');
 
+    // Check if table exists, if not create one
     checkTableExist();
 
-	socket.on('post', function(mList) {
+	socket.on('post', function(name, mList) {
         if(!mList) {
             console.log("There is no points");
             return;
         }
-        mList.forEach(function(point) {
-            var lat = point.lat, lng = point.lng, alt = point.alt;
-            connection.query('INSERT INTO points (lat, lng, alt) VALUES (?, ?, ?);',
-                [lat, lng, alt],
-                function(err, result) {
-                    console.log("POSTED!, Affected row:" + result.affectedRows);
-                    // socket.emit('respond', result, point);
-                }
-            );
-        });
+        var points = JSON.stringify(mList);
+        connection.query('INSERT INTO points (name, points) VALUES (?, ?);',
+            [name, points],
+            function(err, result) {
+                console.log("POSTED!, Affected row:" + result.affectedRows);
+            }
+        );
 	});
 
     socket.on('request', function() {
@@ -62,9 +60,8 @@ var checkTableExist = function() {
         if(err) {
             connection.query('CREATE TABLE Points (' +
                 'id INT NOT NULL AUTO_INCREMENT,' +
-                'lat DOUBlE NOT NULL,' +
-                'lng DOUBlE NOT NULL,' +
-                'alt DOUBlE NOT NULL,' +
+                'name TINYTEXT NOT NULL,' +
+                'points LONGTEXT NOT NULL,' +
                 'PRIMARY KEY (id)' +
                 ');'
             , (err, result) => {
@@ -75,90 +72,6 @@ var checkTableExist = function() {
         }
     });
 }
-
-
-
-// app.get('/', function (req, res) {
-// 	res.send('<html><body><p>Storytracker</p></body></html>');
-// });
-
-// app.post('/points/put/:id', function (req,res) {
-// 	var id = req.params.id, response = [];
-
-// 	if (
-// 		typeof req.body.x !== 'undefined' &&
-// 		typeof req.body.y !== 'undefined' &&
-// 		typeof req.body.z !== 'undefined'
-// 	) {
-// 		var x = req.body.x, y = req.body.y, z = req.body.z;
-
-// 		connection.query('INSERT INTO points VALUES (?, ?, ?, ?);',
-// 			[id, x, y, z],
-// 			function(err, result) {
-// 		  		if (!err){
-
-// 					if (result.affectedRows != 0) {
-// 						response.push({'result' : 'success'});
-// 					} else {
-// 						response.push({'msg' : 'No Result Found'});
-// 					}
-
-// 					res.setHeader('Content-Type', 'application/json');
-// 			    	res.status(200).send(JSON.stringify(response));
-// 		  		} else {
-// 				    res.status(400).send(err);
-// 			  	}
-// 			}
-// 		);
-
-// 	} else {
-// 		response.push({'result' : 'error', 'msg' : 'Please fill required details'});
-// 		res.setHeader('Content-Type', 'application/json');
-//     	res.status(200).send(JSON.stringify(response));
-// 	}
-// });
-
-// // app.delete('/product/delete/:id', function (req,res) {
-// // 	var id = req.params.id;
-
-// // 	connection.query('DELETE FROM nd_products WHERE id = ?', [id], function(err, result) {
-// //   		if (!err){
-// //   			var response = [];
-
-// // 			if (result.affectedRows != 0) {
-// // 				response.push({'result' : 'success'});
-// // 			} else {
-// // 				response.push({'msg' : 'No Result Found'});
-// // 			}
-
-// // 			res.setHeader('Content-Type', 'application/json');
-// // 	    	res.status(200).send(JSON.stringify(response));
-// //   		} else {
-// // 		    res.status(400).send(err);
-// // 	  	}
-// // 	});
-// // });
-
-// app.get('/points/:id', function (req,res) {
-// 	var id = req.params.id;
-
-// 	connection.query('SELECT * from points where id = ?', [id], function(err, rows, fields) {
-//   		if (!err){
-//   			var response = [];
-
-// 			if (rows.length != 0) {
-// 				response.push({'result' : 'success', 'data' : rows});
-// 			} else {
-// 				response.push({'result' : 'error', 'msg' : 'No Results Found'});
-// 			}
-
-// 			res.setHeader('Content-Type', 'application/json');
-// 	    	res.status(200).send(JSON.stringify(response));
-//   		} else {
-// 		    res.status(400).send(err);
-// 	  	}
-// 	});
-// });
 
 http.listen(3000, function(){
 	console.log('Server listening on port: *:3000');
